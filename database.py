d79d7254168e0dacb20fb73f633a34eadf2cc451
Francisco Dottavio <frandottaviosis@gmail.com>
2026-06-12 22:47:58 -0300

    agregue endpoints de registros y login con JWT

--- database.py
+++ database.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+#nos da variables del archivo .env para no exponer datos sensibles
 load_dotenv()
 
+#lee la URL desde el .env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL) #motor de conexión, conecta a PostgreSQL
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from database import engine
+import models
+from routers import router
+
+#SQLalchemy revisa los modelos que existe y crea sus tablas en postgret si no existen, cada vez que comienza la API
+# Llamamos a el motor de conexion
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Conectamos el router con los endpoints de usuarios
+app.include_router(router)
 
 @app.get("/ping")
 def ping():
